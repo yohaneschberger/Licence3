87db2b34bc5814a45197e5b01d58cbdcba352bda
yohaneschberger <yohan.eschberger80000@gmail.com>
2024-07-05 16:53:41 +0200

    Commit Licence
    TP réalisé dans la 3ème année

--- I63/Projet2/Lumiere.py
+++
@@ -1,9 +0,0 @@
-class Lumiere:
-    '''
-    Classe pour les lumières
-        position : Vecteur3D
-        couleur : Couleur
-    '''
-    def __init__(self, position, couleur):
-        self.position = position
-        self.couleur = couleur
